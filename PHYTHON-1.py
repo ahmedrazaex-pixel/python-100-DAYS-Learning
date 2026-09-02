dd1121eb970176f5fdff5058f598817420305cc4
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-02 22:11:54 +0500

    Add files via upload
    I AM WORKING ON MY SKILLS TO BECOME A GOOD Ai Engg and TODAY IS DAY 1

---
+++ PHYTHON-1.py
@@ -0,0 +1,44 @@
+print("ahmed raza")
+print("my name is ahmed raza")
+print(25+25)
+print("my age is 23")
+print(23)
+name="ahmed raza"
+age= 23
+price= 50
+print(age)
+print(price)
+print("my name is:",name)
+print("my shoe price is",price)
+name3ZZ=name
+print("name3ZZ:")
+print('HELLO WORLD')
+print("hello world")
+print("ahmed is my name")
+print("my age is 24")
+print("my name is ahmed raza","my age is 24")
+print("my name is ahmed raza and i will be Ai engg")
+print(24)
+print ('25')
+name="ahmed raza"
+age=2
+age="23.55"
+print(age)
+print("my age is ;",age)
+print("my elephant is white")
+print('23+25=48')
+print("25+50+75=150")
+age = 25
+print(type(name))
+print(type(age))
+print(type(price))
+name1="sk"
+name2="ahmed"
+name3='raza' 
+name="raza" 
+old=False
+a=None
+print(type(old))
+a=2
+b=5
+sum=a+b
