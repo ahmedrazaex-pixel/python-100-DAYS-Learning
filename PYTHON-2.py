d98548b0cd910676eff8ba0acaaf1851d619ceb3
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-03 22:07:39 +0500

    Add files via upload

---
+++ PYTHON-2.py
@@ -0,0 +1,55 @@
+age = 23
+print (age)
+a = 5
+b = 6
+sum=a+b
+print(sum)
+c=10
+d=20
+sub=d-c
+print(sub)
+e=15
+f=5
+mul=e*f
+print(mul)
+g=100
+h=48
+div=g/h
+print(div)
+i=7
+j=4
+mod=i%j
+print(mod)
+k=2
+l=4
+pow=k**l
+print(pow)
+m=10
+n=3
+floor_div=m//n
+print(floor_div)
+o=15
+p=4
+floor_div2=o//p
+print(floor_div2)
+q=20
+r=6
+floor_div3=q//r
+print(floor_div3)
+s=25
+t=7
+floor_div4=s//t
+print(floor_div4)
+u=3
+v=9
+floor_div5=u//v
+print(floor_div5)
+w=8
+x=2
+floor_div6=w//x
+print(floor_div6)
+y=14
+z=5
+floor_div7=y//z
+print(floor_div7)
+
