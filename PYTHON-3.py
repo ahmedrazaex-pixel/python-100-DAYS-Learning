9d242a2f6607b82430c19d5316dbb1ab19fc6df2
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-04 22:27:19 +0500

    Add files via upload
    DAY 3 of PYTHON Ai Engg journey

---
+++ PYTHON-3.py
@@ -0,0 +1,27 @@
+a=2
+b=10
+print(sum)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+#i am learning python
+a==b
+#relationnal operator
+#TRUE FALSE
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+#ASSIGNMENT OPERATOR
+num=20
+num=num+num+20
+print(num+num+20)
+num+=20
+#LOGICAL OPERATOR
+print(a>1 and b>1)
