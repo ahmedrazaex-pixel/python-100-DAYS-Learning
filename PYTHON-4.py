2bc1bf41e856737e1fda9277906b7783bf4a0e6c
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-05 22:25:02 +0500

    Add files via upload
    python learning journey day 4
    aim to become Ai Engg

---
+++ PYTHON-4.py
@@ -0,0 +1,15 @@
+# logical operator
+a=50
+b=30
+print(not False)
+print(not(a>b))
+val1=True
+val2=False
+print(val1 and val2)
+print(val1 or val2)
+print(val1 and not val2)
+print(val1 or not val2)
+c=2
+d=3.20
+sum=c+d
+print(sum)
