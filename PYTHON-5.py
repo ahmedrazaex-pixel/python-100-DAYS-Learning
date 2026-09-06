ed645257dcc58569577c539e2e3ac9fc66dc7b26
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-06 21:55:02 +0500

    Add files via upload

---
+++ PYTHON-5.py
@@ -0,0 +1,12 @@
+input("enter your name; python")
+input("enter your age; 24")
+name=input("enter your marks;94.5")
+print('welcome;name')
+print('you entered',name)
+#----------------------
+name=input("enter name;")
+age=input('enter age;')
+marks=input('enter marks;')
+print('welcome;name')
+#--------------------------------------------------
+first=int(input('enter first;'))
