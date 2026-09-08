00e79d6e5c7cdfd64486ccd12c7f09c92ac052e8
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-08 22:59:49 +0500

    Add files via upload

---
+++ PYTHON-7.py
@@ -0,0 +1,10 @@
+str1="hello,world"
+str2 = 'python'
+str3="this is multi line string"
+str4="this is ahmed's pc"
+str5='this is a string.\n we are creating it in python.'
+print(str5)
+str6="this is single line string\t this is in python."
+#concatenation of strings
+str7=str1+str2
+print(str7)
