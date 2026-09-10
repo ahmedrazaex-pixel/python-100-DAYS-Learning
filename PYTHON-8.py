de1ed0afb2430114bb429820f6d21d17bd9b2e9e
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-10 13:41:07 +0500

    Add files via upload

---
+++ PYTHON-8.py
@@ -0,0 +1,15 @@
+str1='apna'
+len1=len(str1)
+print(len1)
+str2='naam'
+len2=len(str2)
+print(len2)
+str3='kya hai'
+len3=len(str3)
+print(len3)
+print(str1+str2+str3)
+final=str(str1+""+str3)
+str='ahmed raza'
+print(str[3])
+print(str[1])
+print(str[5])
