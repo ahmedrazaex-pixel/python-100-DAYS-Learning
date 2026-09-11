0aebfdf3d2fc5329e3ba42fbc3bbd19647b63e7b
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-11 21:32:05 +0500

    Add files via upload

---
+++ PYTHON-10.py
@@ -0,0 +1,23 @@
+str='mango'
+print(str[-3:-1])
+print(str[3:1])
+str='i am working to be an Ai engineer'
+print(str[0:5])
+print(str[-3:-2])
+#str.endswith
+print(str.endswith('neer'))
+#str.startswith
+print(str.startswith('i am'))
+#str.capitalize
+print(str.capitalize())
+str1=str.capitalize()
+print(str1)
+#str.replace
+print(str.replace('Ai','Oi'))
+print(str.replace('Ai','Ahmed'))
+#str.find
+print(str.find('Ai'))
+print(str.find('engineer'))
+#str.count
+print(str.count('i'))
+print(str.count('a'))
