74b2ce87c8004535f7a10cc16b05c578c11fd3cc
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-12 21:36:42 +0500

    Add files via upload

---
+++ PYTHON-11.py
@@ -0,0 +1,10 @@
+input('enter your name:')
+str="hi,i am the $ symbol $50.5" 
+print(str.count('$'))
+#conditional statements
+#if
+age=24
+if(age>=18):
+    print('you are eligible to vote')
+    if(True):
+        print('you are eligible to vote')
