1440fa7b31feb031cbd3867a5ba45f343b7c25d7
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-13 21:27:06 +0500

    Add files via upload

---
+++ PYTHON-12.py
@@ -0,0 +1,15 @@
+light='green'
+if(light=='red'):
+    print('stop')
+elif(light=='yellow'):
+    print('get ready')
+else:
+    print('go')
+#CONDITIONAL STATEMENT
+num=5
+if(num > 2):
+    print('num is greater than 2')
+elif(num>3):
+    print('num is greater than 3')
+else:
+    print('light is broken')
