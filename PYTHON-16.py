826f477d149e9b8eeed494fab78a3b184765c34c
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-17 21:39:50 +0500

    Add files via upload

---
+++ PYTHON-16.py
@@ -0,0 +1,17 @@
+marks=[85,25,88,63,45]
+print(marks[1:3])
+print(marks[1:])
+print(-3,-1)
+list=[2,1,3]
+#LIST METHODS
+#list.append
+list.append(4)
+print(list)
+list.append(6)
+print(list)
+#list.sort
+print(list.sort())
+print(list)
+print(list.sort(reverse=True))
+list=['banana','mango','honey']
+print(list.sort(reverse=False))
