65d49a3188d0f36794ced5b48af90bbbba6a56e5
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-19 23:07:48 +0500

    Implement list operations in PYTHON-18.py

---
+++ PYTHON-18.py
@@ -0,0 +1,14 @@
+list=['a','d','c','b']
+list.reverse()
+print(list)
+#list.insert
+list=[2,1,3]
+list.insert(1,5)
+print(list)
+#list.remove
+list.remove(1)
+print(list)
+ #list.pop
+list.pop(2)
+print(list)
+
