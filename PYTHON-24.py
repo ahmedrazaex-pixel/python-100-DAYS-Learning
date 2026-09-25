24e8c1ba9aa42f26fb0469eafe39c6c19a4e637c
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-25 21:51:27 +0500

    Add files via upload

---
+++ PYTHON-24.py
@@ -0,0 +1,18 @@
+tup=(1,2,3,4)
+print(tup)
+print(tup[1:3])
+#TUPLE METHOD
+tup=(2,1,3,1)
+#tuple.index
+print(tup.index(2))
+#tuple.count
+print(tup.count(2))
+print(tup.count(1))
+movies=[]
+mov1=input("enter 1st movie:")
+mov2=input("enter 2nd movie:")
+mov3=input("enter 3rd movie:")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
