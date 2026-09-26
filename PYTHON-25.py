bc143d3ad564821ff58a13bac1057dbe42e7ba78
ahmed-raza-EX1 <ahmedrazaex@gmail.com>
2026-09-26 22:00:41 +0500

    Add files via upload

---
+++ PYTHON-25.py
@@ -0,0 +1,12 @@
+movies=[]
+movies.append(input("enter 1st movie:"))
+movies.append(input("enter 2nd movie:"))
+print(movies)
+list=[1,2,3]
+list.copy
+list1=[1,2,1]
+list2=[1,2,3]
+copy_list1=list1.copy()
+copy_list1.reverse()
+if(copy_list1==list1):
+ print("palindrome")
